9baf4e0cd2258bb424744ca9217807f63ca86c96
Горбунов Пётр <pyotr_gorbunov_01@mail.ru>
2021-02-26 00:14:12 +0700

    Окружение

---
+++ VlasovLearn/env2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Pyotr\Desktop\VV-Study-master\VlasovLearn\env2\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
